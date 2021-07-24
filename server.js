6663d9517c65beafc54e335343e6c18d0e921d09
Simba Pfaira <spfaira@yahoo.com>
2021-07-24 14:45:38 +0200

    Level 300 complete

--- server.js
+++ server.js
@@ -3,16 +3,35 @@ const app = express();
 app.use(express.json());
 const Stratford = require('./data/Stratford.json');
 
-app.get('/pharmacies', (req, res) => {
-  res.status(200).json(Stratford.pharmacies);
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .send(
+      'You have reached the London Mini Guide server.... Route to /pharmacies, /doctors, /colleges, /hospitals'
+    );
 });
+
+app.get('/:city/pharmacies', (req, res) => {
+  const { city } = req.params;
+  if (city.toUpperCase() === 'STRATFORD') {
+    res.status(200).json(Stratford.pharmacies);
+  } else {
+    res
+      .status(404)
+      .send(`The data for "${city}" was not found in our database!`);
+  }
+});
+
 app.get('/doctors', (req, res) => {
   res.status(200).json(Stratford.doctors);
 });
+
 app.get('/colleges', (req, res) => {
   res.status(200).json(Stratford.colleges);
 });
+
 app.get('/hospitals', (req, res) => {
   res.status(200).json(Stratford.hospitals);
 });
+
 app.listen(process.env.PORT || 3000, () => console.log('Server started...'));
